63ec585b2d0a360b57a8f838e1346368d5b50bdd
Arkhananta Satrya Ariadilla <103724563+arkhsat@users.noreply.github.com>
2025-02-10 18:44:43 +0700

    Update train_cigarette.py

--- YOLO/train/train_cigarette.py
+++ YOLO/train/train_cigarette.py
@@ -3,7 +3,7 @@ from roboflow import Roboflow
 
 model = YOLO('yolo8n.pt')
 
-rf = Roboflow(api_key="8XE96Iv5aXEqAqX5pqTF")
+rf = Roboflow(api_key="drop your api_key here")
 project = rf.workspace("train-qjr0z").project("detected-b7rrr")
 version = project.version(1)
 dataset = version.download("yolov8")
