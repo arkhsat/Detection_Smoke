8031c2b33b97667e8bcd5c57bca3991c713de1fe
Arkhsat <arkhananta27@gmail.com>
2025-02-10 15:02:52 +0700

    train

--- YOLO/train/train_cigarette.py
+++ YOLO/train/train_cigarette.py
@@ -1,3 +1,9 @@
 from ultralytics import YOLO
+from roboflow import Roboflow
 
-model = YOLO('yolo11n.pt')
+model = YOLO('yolo8n.pt')
+
+rf = Roboflow(api_key="8XE96Iv5aXEqAqX5pqTF")
+project = rf.workspace("train-qjr0z").project("detected-b7rrr")
+version = project.version(1)
+dataset = version.download("yolov8")
