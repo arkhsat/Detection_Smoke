e88913bc4dde9df9de31db892b89c58068b8ff5a
arkhsat <arkhananta27@gmail.com>
2025-05-22 18:54:27 +0700

    Update

--- YOLO/train/train_cigarette.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-from roboflow import Roboflow
-
-model = YOLO('yolo8n.pt')
-
-rf = Roboflow(api_key="drop your api_key here")
-project = rf.workspace("train-qjr0z").project("detected-b7rrr")
-version = project.version(1)
-dataset = version.download("yolov8")
